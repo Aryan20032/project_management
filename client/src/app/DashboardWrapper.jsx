3fc054739ef7bac17d6f34df55d9edb72d4023f0
Aryan Goyal <aryango2307@gmail.com>
2024-09-19 11:36:47 +0530

    d

--- client/src/app/DashboardWrapper.jsx
+++ client/src/app/DashboardWrapper.jsx
@@ -4,9 +4,8 @@ import Sidebar from "./(components)/Sidebar";
 const DashboardWrapper = ({ children }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      sidebar
       <Sidebar />
-      <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50`}>
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg`}>
         <Navbar />
         {children}
       </main>
