7deed0d4507b00c067188f5df91b5993bb15a882
Aryan Goyal <aryango2307@gmail.com>
2024-09-24 21:47:07 +0530

    Update DashboardWrapper.jsx

--- client/src/app/DashboardWrapper.jsx
+++ client/src/app/DashboardWrapper.jsx
@@ -1,8 +1,15 @@
+"use client"
+
 import React from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
-import StoreProvider from "./redux";
+import StoreProvider, { useAppSelector } from "./redux";
 const DashboardLayout = ({ children }) => {
+  const isSidebarCollapsed = useAppSelector((state)=>state.global.isSidebarCollapsed)
+
+  const isDarkMode=useAppSelector((state)=>state.global.isDarkMode)
+
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
