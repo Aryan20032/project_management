97f8c3b32b214ada752594cdf2dbd88739bb34fb
Aryan Goyal <aryango2307@gmail.com>
2024-11-02 20:07:35 +0530

    sd

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  app: [
+  apps: [
     {
       name: "project-management",
       script: "npm",
-      args: "run dev",
+      args: "run dev", // Command to run
+      interpreter: "none", // Ensures PM2 doesn't look for a Node script
       env: {
         NODE_ENV: "development",
       },
